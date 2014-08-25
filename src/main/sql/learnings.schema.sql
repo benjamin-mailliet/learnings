08d72225b87e0dc6adc5173c4eadc5579972f0b2
Benjamin Mailliet <benjamin.mailliet@atos.net>
2014-08-25 18:06:07 +0200

    Ajout des tables ressource et travail Ajout de la version de java dans le pom Ajout du chemin dans Travail

--- src/main/sql/learnings.schema.sql
+++ src/main/sql/learnings.schema.sql
@@ -32,3 +32,33 @@ CREATE TABLE `projettransversal` (
   `datelimiterendulot2` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `ressource` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `chemin` VARCHAR(1000) NOT NULL,
+  `titre` VARCHAR(100) NOT NULL,
+  `cours_id` INTEGER,
+  `tp_id` INTEGER,
+  `projettransversal_id` INTEGER,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`cours_id`) REFERENCES cours(`id`),
+  FOREIGN KEY (`tp_id`) REFERENCES tp(`id`),
+  FOREIGN KEY (`projettransversal_id`) REFERENCES projettransversal(`id`)
+);
+
+
+CREATE TABLE `travail` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `chemin` VARCHAR(1000) NOT NULL,
+  `note` INTEGER,
+  `dateRendu` DATETIME NOT NULL,
+  `tp_id` INTEGER,
+  `projettransversal_id` INTEGER,
+  `utilisateur_id1` INTEGER NOT NULL,
+  `utilisateur_id2` INTEGER,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`tp_id`) REFERENCES tp(`id`),
+  FOREIGN KEY (`projettransversal_id`) REFERENCES projettransversal(`id`),
+  FOREIGN KEY (`utilisateur_id1`) REFERENCES utilisateur(`id`),
+  FOREIGN KEY (`utilisateur_id2`) REFERENCES utilisateur(`id`)
+);
