bd38251eb8f12fa01a869995249618020f8330e4
Anthony Charles <anthonycharles@gmail.com>
2016-09-14 11:18:05 +0200

    feat(envoi mails) : finalisation de l'envoi d'emails
    Affichage de la liste dans une popin
    Bouton pour ouvrir client mail

--- src/main/java/learnings/web/webservices/UtilisateurWebservice.java
+++ src/main/java/learnings/web/webservices/UtilisateurWebservice.java
@@ -1,17 +1,10 @@
 package learnings.web.webservices;
 
-import com.google.gson.Gson;
-import learnings.managers.TravailManager;
 import learnings.managers.UtilisateurManager;
 
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-import java.math.BigDecimal;
 
 @Path("/utilisateur")
 public class UtilisateurWebservice {
