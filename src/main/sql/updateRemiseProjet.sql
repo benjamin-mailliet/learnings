35728f88472db1150bb134f25604459f9a79b5a4
Anthony Charles <anthonycharles@gmail.com>
2017-02-09 14:27:09 +0100

    release(2.0)
    Mise à jour version
    Nettoyage scripts SQL

--- src/main/sql/updateRemiseProjet.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `learnings`.`travail` 
-ADD COLUMN `urlRepository` VARCHAR(1000) NULL DEFAULT NULL AFTER `commentaire`;
-
-ALTER TABLE `learnings`.`travail` 
-CHANGE COLUMN `chemin` `chemin` VARCHAR(1000) NULL DEFAULT NULL;
