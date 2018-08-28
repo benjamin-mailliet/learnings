c5f9958d6fcd7b4590d5390e8a03e633920b4d23
Anthony Charles <anthony.charles@santeos.com>
2018-08-28 13:48:32 +0200

    feat(seances) : refonte de la liste des séances
    - refonte graphique
    - ajout de l'indication des TP notés

--- src/main/webapp/js/seances.js
+++ src/main/webapp/js/seances.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 	$(".cours-article .card-body").on('show.bs.collapse', function () {
-		$(this).parent().children(".card-header").children("h4").children("span.icone").removeClass("fa-folder").addClass("fa-folder-open");
+		$(this).parent().children(".card-header").children(".title").children("h2").children("span.icone").removeClass("fa-folder").addClass("fa-folder-open");
+        $(this).parent().addClass("expanded");
 	});
 	
 	$(".cours-article .card-body").on('hide.bs.collapse', function () {
-		$(this).parent().children(".card-header").children("h4").children("span.icone").addClass("fa-folder").removeClass("fa-folder-open");
+		$(this).parent().children(".card-header").children(".title").children("h2").children("span.icone").addClass("fa-folder").removeClass("fa-folder-open");
+        $(this).parent().removeClass("expanded");
 	});
 	
 	$(".tp-article .card-body").on('show.bs.collapse', function () {
 		$(this).parent().addClass("rotated");
+        $(this).parent().addClass("expanded");
 	});
 	$(".tp-article .card-body").on('hide.bs.collapse', function () {
 		$(this).parent().removeClass("rotated");
+        $(this).parent().removeClass("expanded");
 	});
 });
 
